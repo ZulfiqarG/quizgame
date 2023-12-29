7648690218d9ff081ee631a72ccb483c2bcd8e16
DriverBkup <driverprojectsharing2022dec25@gmail.com>
2023-12-29 17:48:30 +0530

    manageAll javascript

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import HomePage from './Components/Pages/HomePage';
+import Footer from './Components/Footer/Footer';
+import { Routes, Route } from "react-router-dom";
+import CreateQuiz from './Components/Pages/CreateNewQuiz/CreateQuiz';
+import MyQuiz from './Components/Pages/MyQuizzes/MyQuiz';
+import PlayQuiz from './Components/Pages/PlayQuiz/PlayQuiz';
+import NotFound from './Components/NotFound';
+import QuizCard from './Components/Pages/PlayQuiz/QuizCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  >
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/create-new" element={<CreateQuiz />} />
+      <Route path="/my-quiz" element={<MyQuiz />} />
+      <Route path="/play-quiz" element={<PlayQuiz />} />
+      <Route path="/quiz" element={<QuizCard />} /> 
+      <Route path='*' element={<NotFound />}/>
+      </Routes>
+     
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+  return (
+    <div className="container">
+    <div className="footer">
+        <div className="row">
+          <a href="https://www.instagram.com/metagetters" target="blank"><i className="fa fa-instagram" /></a>
+          <a href="https://www.youtube.com/channel/UCvfyhOnrLFg75qT1AVQ7aBg" target="blank"><i className="fa fa-youtube" /></a>
+          <a href="https://github.com/kushagra183" target="blank"><i className="fa fa-github" /></a>
+          <a href="https://facebook.com/profile.php?id=100088240313452" target="blank"><i className="fa fa-facebook" /></a>
+        </div>
+       
+        <div className="row">
+          Zulfiqar-Shaikh Copyright © 2023 ZS-Quiz-B - All rights reserved 
+        </div>
+      </div>
+      </div>
+  )
+}
+
+export default Footer
